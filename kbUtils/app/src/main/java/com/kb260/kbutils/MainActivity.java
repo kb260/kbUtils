671b76e722bc1a064c8a04d3d5ac0472e7ba75cb
kb260 <kb261@ooutlook.com>
2018-01-05 15:10:51 +0800

    first submit

---
+++ kbUtils/app/src/main/java/com/kb260/kbutils/MainActivity.java
@@ -0,0 +1,17 @@
+package com.kb260.kbutils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author  KB260
+ * Created on  2018/1/5
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
